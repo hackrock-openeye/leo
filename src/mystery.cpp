c954335932971eddd44e2a1ed3ff8a8f3079e5e4
hackrock-openeye <osint401@gmail.com>
2024-10-02 21:11:55 +0530

    What you seek may not seek you.

---
+++ src/mystery.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+class Goal {
+public:
+    bool isSeekingYou() {
+        // Randomly decide if the goal is seeking you
+        return rand() % 2 == 0;
+    }
+
+    void appear() {
+        std::cout << "The goal has appeared, but it may not be seeking you!" << std::endl;
+    }
+};
+
+class Person {
+public:
+    void seekGoal(Goal& goal) {
+        std::cout << "You are seeking the goal..." << std::endl;
+        if (goal.isSeekingYou()) {
+            std::cout << "Lucky! The goal is also seeking you. You achieved it!" << std::endl;
+        } else {
+            std::cout << "Unfortunately, the goal is not seeking you. Keep trying." << std::endl;
+        }
+    }
+};
+
+int main() {
+    srand(static_cast<unsigned int>(time(0)));  // Seed for randomness
+    
+    Person person;
+    Goal goal;
+    
+    goal.appear();
+    person.seekGoal(goal);
+
+    return 0;
+}
